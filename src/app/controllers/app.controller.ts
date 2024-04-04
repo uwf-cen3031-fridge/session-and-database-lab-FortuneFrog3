1a0823b019237f68518273f03acb9a3fab1a1513
Chase Dittbenner <csd19@students.uwf.edu>
2024-04-04 12:43:12 -0500

    saving progress

--- src/app/controllers/app.controller.ts
+++ src/app/controllers/app.controller.ts
@@ -11,11 +11,40 @@ export class AppController {
 
   private initializeRouter() {
 
+    this.router.get("/login", (req: Request, res: Response) => {
+    res.render("login")
+  });
+
+    this.router.post("/login", (req: any, res: Response)=>{
+      req.session.user = req.body.username;
+      res.redirect("/");
+  });
+
+  this.router.get("/login", (req: any, res: Response)=>{
+    req.session.destroy(() => {
+    res.redirect("/");
+  });
+});
+
+//Protect the homepage
+const enforceLogin = ((req: any, res: Response, next: any)=>{
+  if(req.session.user) {
+   next();
+  } else {
+    res.redirect("/login")
+  }
+});
+
+  //security
+  this.router.use(enforceLogin);
+
     // Serve the home page
-    this.router.get("/", (req: Request, res: Response) => {
+    this.router.get("/", enforceLogin, (req: any, res: Response) => {
       try {
         // Render the "home" template as HTML
-        res.render("home");
+        res.render("home", {
+          user: req.session.user
+        });
       } catch (err) {
         this.log.error(err);
       }
